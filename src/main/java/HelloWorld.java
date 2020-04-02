0adb494b69a5344ec444803c683f6cb1ffbe2825
Cosmin Pitaru <cosmin.pitaru@endava.com>
2020-04-02 17:37:02 +0300

    Add Hello World

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,23 @@
+/*
+ * Classname : PACKAGE_NAME.HelloWorld
+ *
+ * Created on: 02 Apr 2020
+ *
+ * Copyright (c) 2000-2020 Realex Payments, Ltd.
+ * Realex Payments, The Observatory, 7-11 Sir John Rogerson's Quay, Dublin 2, Ireland
+ *
+ * All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * Realex Payments, Ltd. ("Confidential Information"). You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Realex Payments.
+ *
+ */
+
+public class HelloWorld {
+    public static void main(String...a) {
+        System.out.println("Hello world...automation!");
+    }
+}
