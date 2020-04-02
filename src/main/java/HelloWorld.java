57df0fcbefafc2b57071ab48b297532e6c87fd4c
Cosmin Pitaru <cosmin.pitaru@endava.com>
2020-04-02 17:56:14 +0300

    Modifying Hello World

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -3,17 +3,6 @@
  *
  * Created on: 02 Apr 2020
  *
- * Copyright (c) 2000-2020 Realex Payments, Ltd.
- * Realex Payments, The Observatory, 7-11 Sir John Rogerson's Quay, Dublin 2, Ireland
- *
- * All Rights Reserved.
- *
- * This software is the confidential and proprietary information of
- * Realex Payments, Ltd. ("Confidential Information"). You shall not
- * disclose such Confidential Information and shall use it only in
- * accordance with the terms of the license agreement you entered into
- * with Realex Payments.
- *
  */
 
 public class HelloWorld {
